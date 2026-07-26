1dfa131e3ae988b1d59b6c0d6ffa631ad2cc65c3
blubeardtom <bluebeardtom@gmail.com>
2026-07-26 14:53:14 +0900

    2026-07-26-14-53-14

--- js/old.js
+++ js/old.js
@@ -1,3 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+let tab=document.getElementById("bio")
+let htm=
+`
+<div class="text-center text-white bg-primary">bio</div>
 <table class="table align-middle text-center">
 <tbody>
 <tr>
@@ -5,12 +10,15 @@
 <img src="img/wb3.png" alt="Blue Beard" class="img-fluid" style="max-width: 300px;"><br>
 <span class="badge rounded-pill bg-primary px-3 py-2 fs-6 shadow-sm">青髭</span>
 </td>
+
 <td>
 <img src="img/fb3.png" alt="Tom" class="img-fluid" style="max-width: 300px;"><br>
 <span class="badge rounded-pill bg-primary px-3 py-2 fs-6 shadow-sm">トム</span>
 </td>
+
 </tr>
 </tbody>
 </table>
-
-
+`
+tab.innerHTML=htm
+})
