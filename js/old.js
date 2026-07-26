7a5770846dd44ebb58306c1e2993aa94f956021d
blubeardtom <bluebeardtom@gmail.com>
2026-07-26 15:30:48 +0900

    2026-07-26-15-30-48

--- js/old.js
+++ js/old.js
@@ -7,12 +7,20 @@ let htm=
 <tbody>
 <tr>
 <td>
-<img src="img/wb3.png" alt="Blue Beard" class="img-fluid" style="max-width: 300px;"><br>
+<img src="img/old/m0.png" alt="Blue Beard" class="img-fluid" style="max-width: 50px;"><br>
 <span class="badge rounded-pill bg-primary px-3 py-2 fs-6 shadow-sm">青髭</span>
 </td>
 
 <td>
-<img src="img/fb3.png" alt="Tom" class="img-fluid" style="max-width: 300px;"><br>
+<img src="img/old/m1.png" alt="Tom" class="img-fluid" style="max-width: 50px;"><br>
+<span class="badge rounded-pill bg-primary px-3 py-2 fs-6 shadow-sm">トム</span>
+</td>
+<td>
+<img src="img/old/m2.png" alt="Tom" class="img-fluid" style="max-width: 50px;"><br>
+<span class="badge rounded-pill bg-primary px-3 py-2 fs-6 shadow-sm">トム</span>
+</td>
+<td>
+<img src="img/old/m3.png" alt="Tom" class="img-fluid" style="max-width: 50px;"><br>
 <span class="badge rounded-pill bg-primary px-3 py-2 fs-6 shadow-sm">トム</span>
 </td>
 
