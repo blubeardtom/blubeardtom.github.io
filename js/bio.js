fe53af0cea025e5e361f2c6349eae93d30710be4
blubeardtom <bluebeardtom@gmail.com>
2026-07-26 19:05:04 +0900

    2026-07-26-19-05-04

--- js/bio.js
+++ js/bio.js
@@ -11,7 +11,7 @@ let htm=
 </td>
 
 <td>
-<img src="img/fb3.png" alt="Tom" class="img-fluid" style="max-width: 300px;"><br>
+<img src="img/fb4.png" alt="Tom" class="img-fluid" style="max-width: 300px;"><br>
 <span class="badge rounded-pill bg-primary px-3 py-2 fs-6 shadow-sm">トム</span>
 </td>
 
