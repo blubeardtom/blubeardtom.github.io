e3fd955123c5d70cece5e340c7d9e447dfb71ce4
blubeardtom <bluebeardtom@gmail.com>
2026-07-26 13:59:19 +0900

    2026-07-26-13-59-19

--- js/n2.js
+++ js/n2.js
@@ -5,12 +5,14 @@ document.addEventListener('DOMContentLoaded', () => {
       
       <ul class="bg-info navbar-nav ms-auto mb-2 mb-lg-0 d-flex flex-row gap-3">
 
-        <li class="nav-item">
+<li class="nav-item">
 <a class="navbar-brand d-flex align-items-center me-auto" href="index.html">
-        <img src="img/wb3.png" alt="Blue Beard" class="d-inline-block align-text-top me-2" style="max-height: 40px;">
-        <span class="fw-bold">Blue Beard Tom</span>
-      </a>
-        </li>
+<img src="img/wb3.png" alt="Blue Beard" class="d-inline-block align-text-top me-2" style="max-height: 40px;">
+</a>
+</li>
+<li class="nav-item">
+<span class="fw-bold">Blue Beard Tom</span>
+</li>
 
         <li class="nav-item">
           <a class="nav-link text-white fw-medium" href="bio.html">Bio</a>
