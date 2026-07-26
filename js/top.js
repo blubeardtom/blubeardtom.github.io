6e4ea25411a2c55559fec72d13a930dec88af0ef
blubeardtom <bluebeardtom@gmail.com>
2026-07-26 11:16:21 +0900

    2026-07-26-11-16-21

---
+++ js/top.js
@@ -0,0 +1,7 @@
+// Create the div element
+const prayerContainer = document.createElement('div');
+prayerContainer.className = 'p-2 text-center bg-warning';
+prayerContainer.textContent = 'Domine Iesu, dimitte nobis: 主イエスよ、おゆるしください';
+
+// Append it to the document body (or any parent container)
+document.body.appendChild(prayerContainer);
